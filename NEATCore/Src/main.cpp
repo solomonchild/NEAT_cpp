ec0a54a18ab3e839af63bad1b6e0404ae4107f78
solomonchild <solomonchild@gmail.com>
2016-12-05 15:22:36 +0200

    Removed invalid from main.cpp

--- NEATCore/Src/main.cpp
+++ NEATCore/Src/main.cpp
@@ -21,32 +21,8 @@ int main(int argc, char** argv)
         {
             population.emplace_back(Genome (generator));
         }
-        Evaluator eval;
         std::array<Inputs, 4> inputs = {Inputs{1, 0}, Inputs{0, 1}, Inputs{0,0}, Inputs{1,1}};
-        std::array<float, 4> fitness = {};
-        Genome genome(generator);
-        while(true)
-        {
-            std::cout << genome << std::endl;
-            std::array<Outputs, 4> outputs = {};
-            bool to_break = true;
-            for (unsigned i = 0; i < inputs.size(); ++i)
-            {
-                outputs[i] = genome.evaluate_network(inputs[i]);
-                fitness[i] = eval.get_fitness(outputs[i], inputs[i]);
-                if(fitness[i] > 0.8)
-                {
-                    to_break = false;
-                }
-                DEBUG("Fitness: %f", fitness[i]);
-            }
-
-            if(to_break)
-            {
-                break;
-            }
-            genome.mutate();
-        }
+        (void) inputs;
     }
     catch(std::exception* e)
     {
