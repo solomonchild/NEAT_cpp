2b67d0cff7963b96db1f006994ab93cae86f69f8
solomonchild <solomonchild@gmail.com>
2016-12-06 12:08:19 +0200

    Added main loop

--- NEATCore/Src/main.cpp
+++ NEATCore/Src/main.cpp
@@ -11,18 +11,23 @@ int main(int argc, char** argv)
 {
     try
     {
-        // for each genome apply problem
-        // assign each genome to species
-        // for each species, cull genomes
-        // breed each species(randomly select genomes to crossover and/or mutate)
         std::shared_ptr<RandomGenerator> generator = std::make_shared<RandomGenerator>();
-        std::vector<Genome> population;
-        for(unsigned int i = 0; i < Parameters::population_size; ++i)
-        {
-            population.emplace_back(Genome (generator));
-        }
         std::array<Inputs, 4> inputs = {Inputs{1, 0}, Inputs{0, 1}, Inputs{0,0}, Inputs{1,1}};
         (void) inputs;
+        Pool pool(generator);
+
+        while(true)
+        {
+            for(auto& species : pool)
+            {
+                for(auto& genome : species)
+                {
+                    std::cout << genome;
+                }
+            }
+            break;
+        }
+
     }
     catch(std::exception* e)
     {
